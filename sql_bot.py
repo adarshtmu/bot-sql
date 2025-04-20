3654b7a4f76287558211d8b26f350f47eb8046ed
adarsh.ai <123152977+adarshtmu@users.noreply.github.com>
2025-04-20 14:11:13 +0530

    Update sql_bot.py

--- sql_bot.py
+++ sql_bot.py
@@ -23,7 +23,7 @@ hide_streamlit_style = """
         .st-emotion-cache-1jicfl2 {display: none !important;} /* Hides Streamlit's footer */
         /* Increase font size for Start SQL Challenge! button */
         button[kind="primary"] {
-            font-size: 24px !important; /* Adjust font size as needed */
+            font-size: 40px !important; /* Adjust font size as needed */
         }
     </style>
 """
