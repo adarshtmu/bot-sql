19055d4354e44c7049c6144462b96c4a0bde2d5a
adarsh.ai <123152977+adarshtmu@users.noreply.github.com>
2025-06-27 18:11:49 +0530

    Update sql_bot.py

--- sql_bot.py
+++ sql_bot.py
@@ -771,7 +771,7 @@ if not st.session_state.quiz_started:
         border: 1px solid rgba(255, 255, 255, 0.2);
         padding: 1rem 2rem;
         border-radius: 40px;
-        margin: -2rem auto 2rem auto;
+        margin: -6rem auto 1rem auto;
         max-width: 1100px;
         box-shadow: 0 40px 100px rgba(0, 0, 0, 0.3),
                     0 20px 50px rgba(120, 119, 198, 0.2),
